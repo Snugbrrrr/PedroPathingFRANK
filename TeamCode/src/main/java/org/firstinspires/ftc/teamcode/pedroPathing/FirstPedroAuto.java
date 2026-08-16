745933f9d1f7f3994966ab91f59e978dbd908de1
Snugbrrrr <147729455+Snugbrrrr@users.noreply.github.com>
2026-08-16 10:51:21 -0500

    alliance flipping, some fixes, servo testing, this is great :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/FirstPedroAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/FirstPedroAuto.java
@@ -10,27 +10,33 @@ import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name="Test Pedro OP")
 @Configurable
 public class FirstPedroAuto extends OpMode {
 
     // variables
+    private String alliance;
     private Follower follower;
+    private Servo helicopterBoy;
     Timer pathTimer, opModeTimer;
 
     // ‘Pose’ is a Pedro data type / Class (object)
-    private final Pose testPose = new Pose();
-//    private final Pose startPose = new Pose(40, 120, Math.toRadians(140));
-//    private final Pose shootPose = new Pose(50, 110, Math.toRadians(140));
-//    private final Pose stopPose = new Pose(60, 90, Math.toRadians(90));
-    private final Pose startPose = new Pose(20, 120, Math.toRadians(140));
-    private final Pose shootPose = new Pose(60, 80, Math.toRadians(140));
-    private final Pose stopPose = new Pose(72, 140, Math.toRadians(90));
+//    private final Pose startPose = new Pose((alliance.equals("blue")) ? 20 : 144-20, 120, Math.toRadians(140));
+//    private final Pose shootPose = new Pose(33, 108, Math.toRadians(140));
+//    private final Pose pickupPose = new Pose(24, 94, Math.toRadians(180));
+//    private final Pose stopPose = new Pose(45, 113, Math.toRadians(90));
+
+    private Pose startPose;
+    private Pose shootPose;
+    private Pose pickupPose;
+    private Pose stopPose;
+
 
     // ‘enum’ is a data type in Java, somewhat similar to a list
     public enum PathState{
-        drive_start_to_shoot, shoot, drive_shoot_to_stop
+        drive_start_to_shoot, shoot, drive_shoot_to_pickup, drive_pickup_to_stop
     }
 
 
@@ -44,7 +50,7 @@ public class FirstPedroAuto extends OpMode {
     }
 
     // ‘PathChain’ is a Pedro data type that will use the different Poses
-    private PathChain driveStartToShoot, driveShootToStop;
+    private PathChain driveStartToShoot, driveShootToPickup, drivePickupToStop;
 
     // helper function to build the PathChain variables
     public void buildPaths(){
@@ -52,9 +58,13 @@ public class FirstPedroAuto extends OpMode {
                 .addPath(new BezierLine(startPose, shootPose))
                 .setLinearHeadingInterpolation(startPose.getHeading(), shootPose.getHeading())
                 .build();
-        driveShootToStop = follower.pathBuilder()
-                .addPath(new BezierLine(shootPose, stopPose))
-                .setLinearHeadingInterpolation(shootPose.getHeading(), stopPose.getHeading())
+        driveShootToPickup = follower.pathBuilder()
+                .addPath(new BezierLine(shootPose, pickupPose))
+                .setLinearHeadingInterpolation(shootPose.getHeading(), pickupPose.getHeading())
+                .build();
+        drivePickupToStop = follower.pathBuilder()
+                .addPath(new BezierLine(pickupPose, stopPose))
+                .setLinearHeadingInterpolation(pickupPose.getHeading(), stopPose.getHeading())
                 .build();
     }
 
@@ -69,13 +79,21 @@ public class FirstPedroAuto extends OpMode {
             case shoot:
                 telemetry.addLine("SHOOOTING!!!!!!!!!!!!!! RAGGHHHHH!!!!!!!!");
 //                sleep(1000);
+                if(!follower.isBusy()){
+                    helicopterBoy.setPosition(0.5);
+                }
                 if(!follower.isBusy() && pathTimer.getElapsedTimeSeconds() > 5){
-                    setPathState(PathState.drive_shoot_to_stop);
+                    setPathState(PathState.drive_shoot_to_pickup);
                 }
                 break;
-            case drive_shoot_to_stop:
+            case drive_shoot_to_pickup:
                 if(!follower.isBusy()){
-                    follower.followPath(driveShootToStop);
+                    follower.followPath(driveShootToPickup);
+                    setPathState(pathState.drive_pickup_to_stop);
+                }
+            case drive_pickup_to_stop:
+                if(!follower.isBusy()){
+                    follower.followPath(drivePickupToStop);
                 }
             default:
                 telemetry.addLine("No State Command. Ruh roh");
@@ -85,6 +103,20 @@ public class FirstPedroAuto extends OpMode {
 
     @Override
     public void init() {
+        // get pot value, if something, set alliance to red
+        alliance = "red"; // TODO change this to a conditional with pot value
+
+        startPose = new Pose((alliance.equals("blue")) ? 20 : 144-20, 120, Math.toRadians((alliance.equals("blue")) ? 140 : 180-140));
+
+        shootPose = new Pose((alliance.equals("blue")) ? 33 : 144-33, 108, Math.toRadians((alliance.equals("blue")) ? 140 : 180-140));
+
+        pickupPose = new Pose((alliance.equals("blue")) ? 24 : 144-24, 94, Math.toRadians((alliance.equals("blue")) ? 180 : 180-180));
+
+        stopPose = new Pose((alliance.equals("blue")) ? 45 : 144-45, 113, Math.toRadians((alliance.equals("blue")) ? 90 : 180-90));
+
+        helicopterBoy = hardwareMap.get(Servo.class, "helicopterBoy");
+        helicopterBoy.setDirection(Servo.Direction.FORWARD);
+        helicopterBoy.setPosition(0.0);
         pathState = PathState.drive_start_to_shoot;
         pathTimer = new Timer();
         opModeTimer = new Timer();
